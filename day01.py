639813b73770418473aa466a8a53f6c60623ce83
BAAAKA <roy.aregger@students.fhnw.ch>
2024-12-01 12:23:25 +0100

    made day 1 super short

--- day01.py
+++ day01.py
@@ -1,19 +1,8 @@
 import numpy as np
-
 with open('data/day01.txt','r') as f:
     lines = f.read().splitlines()
 
+sorted_leftc, sorted_rightc = np.sort(np.array([list(map(int, v.split())) for v in lines]).T, axis=1)
 
-leftc, rightc = np.array([list(map(int, v.split('   '))) for v in lines]).T
-
-sorted_leftc = np.sort(leftc)
-sorted_rightc = np.sort(rightc)
-
-diffs = sum(abs(sorted_leftc - sorted_rightc))
-print(f'P1: Difference is {diffs}')
-
-unique, counts = np.unique(rightc, return_counts = True)
-right_dict = dict(zip(map(int, unique), map(int, counts)))
-total = sum([v*right_dict.get(v, 0) for v in leftc])
-
-print(f'P2: Sym is {total}')
+print(f'Part 1: Difference is {sum(abs(sorted_leftc - sorted_rightc))}')
+print(f'Part 2: Similarity is {sum(v*dict(zip(*np.unique(sorted_rightc, return_counts=True))).get(v, 0) for v in sorted_leftc)}')
