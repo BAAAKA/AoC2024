5ce5c09706d3861bfb2d216ccf2eb56265963125
BAAAKA <roy.aregger@students.fhnw.ch>
2024-12-01 12:02:46 +0100

    p2

--- day01.py
+++ day01.py
@@ -3,12 +3,17 @@ import numpy as np
 with open('data/day01.txt','r') as f:
     lines = f.read().splitlines()
 
-print(lines)
 
-splitnr = np.array([list(map(int, v.split('   '))) for v in lines])
+leftc, rightc = np.array([list(map(int, v.split('   '))) for v in lines]).T
 
-sorted_splitnr = np.sort(splitnr.T)
+sorted_leftc = np.sort(leftc)
+sorted_rightc = np.sort(rightc)
 
-diffs = sum(abs(sorted_splitnr[0] - sorted_splitnr[1]))
-print(f'Difference is {diffs}')
+diffs = sum(abs(sorted_leftc - sorted_rightc))
+print(f'P1: Difference is {diffs}')
 
+unique, counts = np.unique(rightc, return_counts = True)
+right_dict = dict(zip(map(int, unique), map(int, counts)))
+total = sum([v*right_dict.get(v, 0) for v in leftc])
+
+print(f'P2: Sym is {total}')
