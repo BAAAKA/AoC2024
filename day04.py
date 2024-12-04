fa03b2f3bd1d29953a423b9c9ebbc7281576e0a0
BAAAKA <baaakaDev@protonmail.ch>
2024-12-04 09:17:38 +0100

    cleanup

--- day04.py
+++ day04.py
@@ -3,8 +3,6 @@ import numpy as np
 with open('data/day04.txt','r') as f: 
     lines = f.read().splitlines()
 
-lines = np.array(lines)
-
 def get_cell(r,c):
     if r>=0 and r<len(lines) and c>=0 and c<len(lines[0]):
         return lines[r][c]
@@ -12,20 +10,32 @@ def get_cell(r,c):
 
 def get_xmas(pos=(0,0)):
     count = 0
-    pos = np.array(pos)
-    directions = np.array([(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)])
-    for direction in directions:
+    for direction in np.array([(1,0),(0,1),(1,1),(1,-1)]):
         letters=""
         for i in range(0,4):
             letters += get_cell(*(pos+direction*i))
-        if letters == 'XMAS':
+        if letters in ['XMAS', 'SAMX']:
             count+=1
     return count
 
-totalCount = 0
+def get_x_mas(pos=(0,0)):
+    for direction in np.array([((1,1), (0,0), (-1,-1)),((1,-1), (0,0), (-1,1))]):
+        letters=""
+        for coords in direction:
+            letters+=get_cell(*pos+coords)
+        if letters not in ['MAS', 'SAM']:
+            return 0
+    return 1
 
+totalCount = 0
 for r in range(0,len(lines)):
     for c in range(0,len(lines[0])+1):
         totalCount += get_xmas(pos=(r,c))
+print(f'Part 1: {totalCount}')
 
-print(totalCount)
+totalCount = 0
+for r in range(0,len(lines)):
+    for c in range(0,len(lines[0])+1):
+        totalCount += get_x_mas(pos=(r,c))
+print(f'Part 2: {totalCount}')
+
