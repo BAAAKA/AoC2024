11c8254533e070b1ed6e5bb5670fc4d99b50148d
BAAAKA <roy.aregger@students.fhnw.ch>
2024-12-15 15:33:57 +0100

    Created map p2

--- day15.py
+++ day15.py
@@ -9,59 +9,66 @@ whitespace_index = np.where(np.array(lines) == '')[0][0]
 
 commands = ''.join(lines[whitespace_index+1:])
 store_map = np.array([list(row) for row in lines[:whitespace_index]])
-# wide_store = np.array([])
-# for row in store_map:
-#     print(row)
+wide_store = []
+
+wider_version = {"#" :'##', 'O': '[]', '.': '..', '@': '@.'}
+for row in store_map:
+    wide_store.append(list(''.join([wider_version[symbol] for symbol in row])))
+
+wide_store = np.array(wide_store)
+
+print(wide_store)
     
 
-coords = np.where(store_map=='@')
-curr_pos = np.array(list(map(int, (coords[0][0], coords[1][0]))))
 
-print(store_map)
-print(commands)
-print(curr_pos)
+# coords = np.where(store_map=='@')
+# curr_pos = np.array(list(map(int, (coords[0][0], coords[1][0]))))
 
-def check_behind_boxes(store_map, parsed_command, new_pos):
-    for i in range(100):
-        new_pos = new_pos+parsed_command
-        symbol = store_map[*new_pos]
-        if symbol == '#':
-            return '#'
-        if symbol == '.':
-            return new_pos
+# print(store_map)
+# print(commands)
+# print(curr_pos)
 
-
-for command in commands:
-    parsed_command = directions[command]
-    new_pos = curr_pos+parsed_command
-    symbol = store_map[*new_pos]
-    # print(f'{command} | new_pos: {new_pos} | {store_map[*new_pos]}')
-
-    if symbol == '#':
-        continue
-    if symbol == 'O':
-        next_empty_pos = check_behind_boxes(store_map=store_map, parsed_command=parsed_command, new_pos=new_pos)
-        if isinstance(next_empty_pos, str) and next_empty_pos=='#':
-            continue
-        store_map[*next_empty_pos] = 'O'
-        store_map[*curr_pos] = '.'
-        store_map[*new_pos] = '@'
-        curr_pos = new_pos
-    else:
-        store_map[*curr_pos] = '.'
-        store_map[*new_pos] = '@'
-        curr_pos = new_pos
-
-    # print(store_map)
-
-total_value = 0
-for r in range(len(store_map)):
-    for c in range(len(store_map[0])):
-        if store_map[r][c] == 'O':
-            total_value += r*100+c
+# def check_behind_boxes(store_map, parsed_command, new_pos):
+#     for i in range(100):
+#         new_pos = new_pos+parsed_command
+#         symbol = store_map[*new_pos]
+#         if symbol == '#':
+#             return '#'
+#         if symbol == '.':
+#             return new_pos
 
 
+# for command in commands:
+#     parsed_command = directions[command]
+#     new_pos = curr_pos+parsed_command
+#     symbol = store_map[*new_pos]
+#     # print(f'{command} | new_pos: {new_pos} | {store_map[*new_pos]}')
+
+#     if symbol == '#':
+#         continue
+#     if symbol == 'O':
+#         next_empty_pos = check_behind_boxes(store_map=store_map, parsed_command=parsed_command, new_pos=new_pos)
+#         if isinstance(next_empty_pos, str) and next_empty_pos=='#':
+#             continue
+#         store_map[*next_empty_pos] = 'O'
+#         store_map[*curr_pos] = '.'
+#         store_map[*new_pos] = '@'
+#         curr_pos = new_pos
+#     else:
+#         store_map[*curr_pos] = '.'
+#         store_map[*new_pos] = '@'
+#         curr_pos = new_pos
+
+#     # print(store_map)
+
+# total_value = 0
+# for r in range(len(store_map)):
+#     for c in range(len(store_map[0])):
+#         if store_map[r][c] == 'O':
+#             total_value += r*100+c
 
 
-print(f'P1: {total_value}')
+
+
+# print(f'P1: {total_value}')
 
