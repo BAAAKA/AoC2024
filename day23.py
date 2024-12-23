33f408bce7a70dbaf60415f1cde569e4746e4707
BAAAKA <baaakaDev@protonmail.ch>
2024-12-23 13:34:27 +0100

    day 23 p2

--- day23.py
+++ day23.py
@@ -12,16 +12,45 @@ for n,m in lines:
     network[m].add(n)
 
 
-combinations = set()
-for key in network:
-    if 't' == key[0]:
-        print(f'{key}: {network[key]}')
+def get_comb(network):
+    combinations = set()
+    for key in network:
         for n in network[key]:
             for overlap in network[n] & network[key]:
                 combinations.add(tuple(sorted((key, n, overlap))))
+    return combinations
 
-print(combinations)
-print(len(combinations))
+def is_in_all(current_lan, new_pc):
+    for pc in current_lan:
+        if new_pc not in network[pc]:
+            return False
+    return True
 
 
-# That's not the right answer; your answer is too high.  2351
+def get_larger_lan(current_lans):
+    combinations = set()
+    for current_lan in current_lans:
+        pc_candidates = network[current_lan[0]]
+        new_bigger_lan = [tuple(sorted(list(current_lan)+[pc_cand])) for pc_cand in pc_candidates if is_in_all(current_lan, pc_cand)]
+        combinations.update(new_bigger_lan)
+    return combinations
+
+current_lans = [tuple([pc]) for pc in network.keys()]
+for i in range(20   ):
+    if len(current_lans)<20:
+        print(f'{i}, {len(current_lans)}: {current_lans}')
+    else:
+        print(f'{i}, {len(current_lans)}')
+
+    current_lans = get_larger_lan(current_lans)
+    if len(current_lans)==1:
+        result = current_lans.pop()
+        print(f'P2: {','.join(result)}')
+        break
+
+
+
+
+# print(combinations)
+# print(len(combinations))
+
