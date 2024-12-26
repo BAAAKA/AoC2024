a0c05eaf318b75c0173bcb105ece8517cd0f9cf0
BAAAKA <roy.aregger@students.fhnw.ch>
2024-12-26 15:44:34 +0100

    day 21 p1, but no way it is able to solve p2, so gonna skip that for now

--- day21.py
+++ day21.py
@@ -1,5 +1,6 @@
 import numpy as np
 from utils import read_file
+import random as rn
 
 lines = read_file('data/day21.txt')
 
@@ -22,7 +23,7 @@ def get_new_position(position, direction):
 
 
 def get_valid_directions(start, end, empty_position):
-    possible_moves = set()
+    possible_moves = set() # 0 is height, 1 is width
     if start[0]>end[0] and get_new_position(start, '^') != empty_position:
         possible_moves.add('^')
     if start[0]<end[0] and get_new_position(start, 'v') != empty_position:
@@ -32,6 +33,12 @@ def get_valid_directions(start, end, empty_position):
     if start[1]<end[1] and get_new_position(start, '>') != empty_position:
         possible_moves.add('>')
     
+    # if len(possible_moves) == 2:
+    #     if empty_position[0] == start[0]:
+    #         possible_moves = possible_moves - {'<', '>'}
+    #     elif empty_position[1] == start[1]:
+    #         possible_moves = possible_moves - {'v', '^'}
+
     return possible_moves
 
 class Pad:
@@ -43,11 +50,13 @@ class Pad:
             self.pos_to_symbol_dict = dpad_dict
             self.width = 3
             self.height = 4
+            self.pad_ara = dpad
         elif pad_type == 'fpad':
             self.symbol_to_pos_dict = fpad_v_to_coord_dict
             self.pos_to_symbol_dict = fpad_dict
             self.width = 3
             self.height = 2
+            self.pad_ara = fpad
         else:
             print(f'UNKNOWN PAD TYPE {pad_type}')
 
@@ -57,6 +66,15 @@ class Pad:
     def get_position(self):
         return self.symbol_to_pos_dict[self.get_symbol()]
     
+    def print_pad(self):
+        for r in range(len(self.pad_ara)):
+            for c in range(len(self.pad_ara[0])):
+                if (r,c) == self.get_position():
+                    print("\033[1;30;43m" + self.pad_ara[r][c] + "\033[0m", end=' ')
+                else:
+                    print(self.pad_ara[r][c], end=' ')
+            print('')
+
     def move(self, direction):
         new_pos = get_new_position(self.get_position(), direction)
         new_height, new_width = new_pos
@@ -84,29 +102,69 @@ class Pad:
     def __repr__(self):
         return self.__str__()
 
+def get_moves(code):
+    pad_type_array = ['fpad'] + ['dpad']*25
+    pads_array = []
+    for pad_type in pad_type_array:
+        the_pad = Pad(pad_type, 'A')
+        pads_array.append(the_pad)
 
-pad_type_array = ['fpad', 'dpad', 'dpad']
-pads_array = []
-for pad_type in pad_type_array:
-    the_pad = Pad(pad_type, 'A')
-    pads_array.append(the_pad)
+    button_index = 0
+    first_pad_inputs = []
+    for step in range(1000000):
+        targets = [code[button_index]]
+        for i in range(len(pads_array)):
+            possible_targets = pads_array[i].path_to_symbol(targets[-1])
+            # if len(possible_targets)>1:
+            #     print(f'possible_targets: {possible_targets}')
+            # target = possible_targets.pop()
+            target = rn.choice(list(possible_targets))
+            targets.append(target)
+        
+        # print('-----')
+        # for pad in pads_array:
+        #     pad.print_pad()
+        #     print('-----')
 
-fpad = Pad('fpad', 'A')
+        if all(v=='A' for v in targets[1:]):
+            # print(f'Button {targets[0]} has been pressed')
+            button_index+=1
+            if button_index == len(code):
+                first_pad_inputs.append(target[-1])
+                # print('FINISHED')
+                return first_pad_inputs
+            
 
-for step in range(25):
-    targets = ['0']
-    for i in range(len(pads_array)):
-        target = pads_array[i].path_to_symbol(targets[-1]).pop()
-        targets.append(target)
-        if targets[-1] == 'A': # ADJUST THIS, WHEN A BEING PRESSED, ITERATE BACK TO THE FIRST PAD AND AT EACH STEP CHECK IF ITS ALSO A, IF YES -> CONTINUE, IF NO, JUST DO THE STEP AND END
-            print(f'{[pad.get_symbol() for pad in pads_array]} {targets} Pressing {targets[-1]} on the {i} pad, which leads to.. Pressing {targets[-2]} on the {i-1} pad')
-            pads_array[i-1].move(targets[-2])
-            break
+        for i in range(len(pads_array), 0, -1):
+            if targets[i] != 'A':
+                # print(f'{step} {[pad.get_symbol() for pad in pads_array]} {targets} pad {i} will be moved {targets[i]}')
+                pads_array[i-1].move(targets[i])
+                break
+        
+        first_pad_inputs.append(target[-1])
 
-        if i == 2:
-            print(f'{[pad.get_symbol() for pad in pads_array]} {targets} Last pad {i}, pressing {targets[-1]}')
-            pads_array[i].move(targets[-1])
+    
 
 
+input_values = []
+print(lines)
+for line in lines:
+    print(f'Line: {line}')
+    best_solution = (99999999,'')
 
+    for i in range(1):
+        first_pad_inputs = get_moves(line)
+        if len(first_pad_inputs)<best_solution[0]:
+            best_solution=(len(first_pad_inputs), first_pad_inputs)
 
+    input_values.append(best_solution)
+    
+total = 0
+for i, (length, result) in enumerate(input_values) :
+    number = int(''.join(char for char in lines[i] if char.isdigit()))
+    print(f'{length} {lines[i]} {number} '+''.join(result))
+    total += length*number
+
+print(f'Total is : {total}')
+
+# That's not the right answer; your answer is too high. 208196 202648
